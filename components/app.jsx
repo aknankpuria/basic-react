8e359572d0a3c85da4f0962c57b2f8af0e7da1c1
Aslam Khan shah <aksi34012@gmail.com>
2023-04-18 20:58:29 +0530

    component

---
+++ components/app.jsx
@@ -0,0 +1,13 @@
+import { header} from './header';
+const React = require('react');
+
+
+function App() {
+    return (
+    <div>
+      <header />
+    </div>
+  );
+}
+
+export default App;
---
+++ components/header.jsx
@@ -0,0 +1,9 @@
+const React = require('react');
+
+function header() {
+  return (
+    <div className="header">
+      <h1>DEV TOWN</h1>
+    </div>
+  );
+}
